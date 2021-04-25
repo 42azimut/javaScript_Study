ef41c94fb770ac4031ba9a978fac6c2d9faa29d6
42azimut <azimutuniverse@gmail.com>
2021-04-25 19:32:47 +0900

    2 constructor fn done

--- intermediate_js/02_constructor_fn.js
+++ intermediate_js/02_constructor_fn.js
@@ -1,3 +1,5 @@
+// 파이썬 인스턴스 객체 만들기와 비슷! 용어혼동 주의!
+
 function User (name, age) {
   this.name = name;
   this.age = age;
@@ -25,10 +27,28 @@ new User();
 function User2 (name, age) {
   this.name = name;
   this.age = age;
-  this.sayName = () => {
-    console.log(this.name);
+  this.sayName = function() {
+    console.log(this.name);  //여기 this는 아래 user3 를 가리킴! 
   }
 }
 let user3 = new User2('kkk', 22);
-console.log(user3);
 user3.sayName();
+
+
+// Item 으로 생성자 함수 상품 객체 생성해보기
+function Item (title, price) {
+  // this = {};
+  this.title = title;
+  this.price = price;
+  this.showPrice = function () {
+    console.log(`이 상품 ${title}의 가격은 ${price} 입니다.`);
+  }
+};
+item1 = new Item ('apple Watch', '230,000');
+item2 = new Item ('apple TAG', '60,000');
+item3 = new Item ('keyboard', '1,538,120');
+item1.showPrice();
+item2.showPrice();
+item3.showPrice();
+
+console.log(item1, item2, item3);
