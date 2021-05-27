e06ffe1ba52e7155aaf8bf15728113fd38f0ff9b
42azimut <azimutuniverse@gmail.com>
2021-05-27 21:19:00 +0900

    02_counter review again

--- Build_15_Vanilla_js/02_counter/app.js
+++ Build_15_Vanilla_js/02_counter/app.js
@@ -5,16 +5,20 @@ let count = 0;
 const value = document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
 
-btns.forEach(function (btn) {
+btns.forEach( (btn) => {
   btn.addEventListener('click', (e) => {
+    //console.log(e.currentTarget.classList);
     const styles = e.currentTarget.classList;
-    if(styles.contains('decrease')) {   //contains()
+
+    if(styles.contains('decrease')) {   //contains()  true or false
       count--;
+      value.style.color = 'blue';
     } else if (styles.contains('increase')) {
       count++;
     } else {
       count = 0;
     }
+
     if (count > 0) {
       value.style.color = 'blue';
     }
